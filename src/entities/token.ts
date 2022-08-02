6cd90a1b00c0d4a56d4c6e2d21af2b099aaa464e
Richard <renyanduo@163.com>
2022-08-02 14:50:04 +0800

    update version

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -80,5 +80,5 @@ export const WETH = {
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.PlianChild0]: new Token(ChainId.PlianChild0, '0xA7615a7F8CBb29359ce292a483618f3868bD928d', 18, 'WPI', 'Wrapped PI')
+  [ChainId.PlianChild0]: new Token(ChainId.PlianChild0, '0xA7615a7F8CBb29359ce292a483618f3868bD928d', 18, 'WETH', 'Wrapped Ether')
 }
